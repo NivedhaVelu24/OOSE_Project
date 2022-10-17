ffb7d8525cc63d5ce8adb3d3bcc31027062274c4
jo3leb3y <joelbellapurla@gmail.com>
2022-10-17 16:44:48 -0400

    login page validation

--- script.js
+++ script.js
@@ -3,14 +3,15 @@ const password = document.getElementById("password");
 const form = document.getElementById("form");
 const error = document.getElementById("error");
 
+
 form.addEventListener('submit', (e) => {
     let messages = [];
     if (email.value === ''|| email.value == null){
         messages.push("email is required");
     }
     
-    if(password.value.length <= 0){
-        messages.push("Password must be longer than 6 characters");
+    if(password.value.length <= 8){
+        alert("Password must be longer than 8 characters");
     }
 
     if(messages.length > 0){
@@ -18,3 +19,18 @@ form.addEventListener('submit', (e) => {
     error.innerText = messages.join(', ');
     }
 })
+
+
+
+// function validate(){
+//     const email = document.getElementById("email");
+//     const password = document.getElementById("password");
+
+
+
+//     if(email.value.trim() == ""||password.value.trim() == ""){
+//         alert("Enter email and password");
+//         return false;
+//     }
+
+// }
